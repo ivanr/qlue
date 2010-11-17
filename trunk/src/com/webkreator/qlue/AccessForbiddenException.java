024a716b230e344e2a90e303e5bdd349dd5153ed
ribicb <ribicb@bdac6999-0aa8-47e0-96f5-c1f392b1c585>
2010-11-17 19:59:27 +0000

    removing // comment

--- trunk/src/com/webkreator/qlue/AccessForbiddenException.java
+++ trunk/src/com/webkreator/qlue/AccessForbiddenException.java
@@ -2,6 +2,6 @@ package com.webkreator.qlue;
 
 public class AccessForbiddenException extends QlueException {
 
-	private static final long serialVersionUID = 1L; //
+	private static final long serialVersionUID = 1L;
 
 }
