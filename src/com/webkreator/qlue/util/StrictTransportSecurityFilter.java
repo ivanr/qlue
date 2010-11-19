ea09f67cc38a19f5011bfca05d61ec76eed00923
ivanr <ivanr@bdac6999-0aa8-47e0-96f5-c1f392b1c585>
2010-11-19 21:48:46 +0000

    Implement Strict Transport Security via a Servlet filter.

---
+++ src/com/webkreator/qlue/util/StrictTransportSecurityFilter.java
@@ -0,0 +1,39 @@
+package com.webkreator.qlue.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class StrictTransportSecurityFilter implements Filter {
+
+	public static final String PARAM_STS_HEADER = "STS_HEADER";
+
+	private String stsHeader;
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		stsHeader = filterConfig.getInitParameter(PARAM_STS_HEADER);
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {		
+		if (stsHeader != null) {
+			((HttpServletResponse) response).setHeader(
+					"Strict-Transport-Security", stsHeader);
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// Nothing to do here
+	}
+}
