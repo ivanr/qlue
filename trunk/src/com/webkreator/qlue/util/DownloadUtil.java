02ed37df95e49e6140ea1bb90296536f76875597
ivanr <ivanr@bdac6999-0aa8-47e0-96f5-c1f392b1c585>
2011-07-10 08:48:08 +0000

    Support 304 responses when downloading static files.

--- trunk/src/com/webkreator/qlue/util/DownloadUtil.java
+++ trunk/src/com/webkreator/qlue/util/DownloadUtil.java
@@ -19,15 +19,23 @@ package com.webkreator.qlue.util;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.OutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.CharacterIterator;
 import java.text.StringCharacterIterator;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.codec.binary.Base64;
+
+import com.webkreator.qlue.PageNotFoundException;
+import com.webkreator.qlue.TransactionContext;
+
 /**
- * This utility class can send a file from the filesystem, 
- * either inline or as an attachment.
+ * This utility class can send a file from the filesystem, either inline or as
+ * an attachment.
  */
 public class DownloadUtil {
 
@@ -38,9 +46,9 @@ public class DownloadUtil {
 	 * @param f
 	 * @throws Exception
 	 */
-	public static void sendFile(HttpServletResponse response, File f)
+	public static void sendFile(TransactionContext context, File f)
 			throws Exception {
-		sendFile(response, f, null, null, false);
+		sendFile(context, f, null, null, false);
 	}
 
 	/**
@@ -50,68 +58,119 @@ public class DownloadUtil {
 	 * @param f
 	 * @throws Exception
 	 */
-	public static void sendFile(HttpServletResponse response, File f,
-			String contentType, String name, boolean isAttachment) throws Exception {
-		// If a content type was provided, use it;
-		// otherwise use our map to set the correct one
-		if (contentType == null) {
-			int i = f.getName().lastIndexOf(".");
-			if (i != -1) {
-				String suffix = f.getName().substring(i + 1);
-				contentType = MimeTypes.getMimeType(suffix);
+	public static void sendFile(TransactionContext context, File f,
+			String contentType, String name, boolean isAttachment)
+			throws Exception {
+		try {
+			// If C-T was not provided, try to use file
+			// extension to select the correct value
+			if (contentType == null) {
+				int i = f.getName().lastIndexOf(".");
+				if (i != -1) {
+					String suffix = f.getName().substring(i + 1);
+					contentType = MimeTypes.getMimeType(suffix);
+				}
 			}
-		}
 
-		// Set C-T, if we have it
-		if (contentType != null) {
-			response.setContentType(contentType);
-		}
+			// Set C-T, if we have it
+			if (contentType != null) {
+				context.response.setContentType(contentType);
+			}
 
-		if (name != null) {
-			// Do not allow control characters in the name
-			StringBuffer sb = new StringBuffer();
-			CharacterIterator it = new StringCharacterIterator(name);
-			for (char c = it.first(); c != CharacterIterator.DONE; c = it
-					.next()) {
-				if (c < 0x20) {
-					throw new SecurityException(
-							"Invalid character in filename: " + c);
+			// Send file name in C-D header
+			if (name != null) {
+				// Do not allow control characters in the name
+				StringBuffer sb = new StringBuffer();
+				CharacterIterator it = new StringCharacterIterator(name);
+				for (char c = it.first(); c != CharacterIterator.DONE; c = it
+						.next()) {
+					if (c < 0x20) {
+						throw new SecurityException(
+								"Invalid character in filename: " + c);
+					}
+
+					if ((c == '\\') || (c == '"')) {
+						sb.append('\\');
+						sb.append(c);
+					} else {
+						sb.append(c);
+					}
 				}
 
-				if ((c == '\\') || (c == '"')) {
-					sb.append('\\');
-					sb.append(c);
+				String escapedName = sb.toString();
+
+				// Set name
+				if (isAttachment) {
+					context.response.setHeader("Content-Disposition",
+							"attachment; filename=\"" + escapedName + "\"");
 				} else {
-					sb.append(c);
+					context.response.setHeader("Content-Disposition",
+							"inline; filename=\"" + escapedName + "\"");
 				}
 			}
 
-			String escapedName = sb.toString();
+			// Prepare data
+			String filename = f.getAbsolutePath();
+			long length = f.length();
+			long lastModified = f.lastModified();
+			String eTag = constructHash(filename + "_" + length + "_"
+					+ lastModified);
 
-			// Set name
-			if (isAttachment) {
-				response.setHeader("Content-Disposition",
-						"attachment; filename=" + escapedName);
-			} else {
-				response.setHeader("Content-Disposition", "inline; filename="
-						+ escapedName);
+			// Check If-None-Match to determine if we can respond with 304
+			String ifNoneMatch = context.request.getHeader("If-None-Match");
+			if ((ifNoneMatch != null)
+					&& ((ifNoneMatch.compareTo("*") == 0) || (ifNoneMatch
+							.compareTo(eTag) == 0))) {
+				context.response.setHeader("ETag", eTag);
+				context.response.sendError(HttpServletResponse.SC_NOT_MODIFIED);
+				return;
 			}
+
+			// Check If-Modified-Since to determine if we can respond with 304
+			long ifModifiedSince = context.request
+					.getDateHeader("If-Modified-Since");
+			if ((ifNoneMatch == null)
+					&& ((ifModifiedSince != -1) && (ifModifiedSince + 1000 > lastModified))) {
+				context.response.setHeader("ETag", eTag);
+				context.response.sendError(HttpServletResponse.SC_NOT_MODIFIED);
+				return;
+			}
+
+			// Set size
+			// TODO How do you deliver a file that's
+			// longer than MAX_INT bytes?
+			context.response.setContentLength((int) length);
+			context.response.setDateHeader("Last-Modified", lastModified);
+			context.response.setHeader("ETag", eTag);
+
+			// Send data
+			OutputStream os = context.response.getOutputStream();
+			BufferedInputStream bis = new BufferedInputStream(
+					new FileInputStream(f));
+			byte b[] = new byte[1024];
+
+			while (bis.read(b) > 0) {
+				os.write(b);
+			}
+
+			bis.close();
+			os.close();
+		} catch (FileNotFoundException e) {
+			// TODO Log reason
+			throw new PageNotFoundException();
+		}
+	}
+
+	public static String constructHash(String input) {
+		MessageDigest md = null;
+
+		try {
+			md = MessageDigest.getInstance("SHA-1");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Log
+			e.printStackTrace();
 		}
 
-		// Set size
-		response.setContentLength((int) f.length());
-
-		// Send data
-		OutputStream os = response.getOutputStream();
-		BufferedInputStream bis = new BufferedInputStream(
-				new FileInputStream(f));
-		byte b[] = new byte[1024];
-
-		while (bis.read(b) > 0) {
-			os.write(b);
-		}
-
-		bis.close();
-		os.close();
+		return Base64.encodeBase64URLSafeString(md.digest(input.getBytes()));
 	}
 }
