71bcaadc798fac1eca5190a261772b4a1920a2d5
ivanr <ivanr@bdac6999-0aa8-47e0-96f5-c1f392b1c585>
2013-07-12 11:56:07 +0000

    Tidy up.

--- src/com/webkreator/qlue/view/NullView.java
+++ src/com/webkreator/qlue/view/NullView.java
@@ -4,7 +4,7 @@ import com.webkreator.qlue.Page;
 import com.webkreator.qlue.TransactionContext;
 
 /**
- * This class is used to indicate that no further output should be made. 
+ * This class is used to indicate that no further output should be made.
  */
 public class NullView implements View {
 
@@ -14,7 +14,7 @@ public class NullView implements View {
 	@Override
 	public void render(TransactionContext tx, Page page) throws Exception {
 		// Do nothing. This method is never invoked.
-				throw new RuntimeException(
-						"Qlue: This method shouldn't have been invoked.");
+		throw new RuntimeException(
+				"Qlue: This method shouldn't have been invoked.");
 	}
 }
