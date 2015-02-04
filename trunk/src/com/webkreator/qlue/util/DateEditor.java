7c08138547ef269af7c8bb2c30bba46f593dc7aa
ivanr <ivanr@bdac6999-0aa8-47e0-96f5-c1f392b1c585>
2015-02-04 14:53:55 +0000

    Add DateEditor.

---
+++ trunk/src/com/webkreator/qlue/util/DateEditor.java
@@ -0,0 +1,37 @@
+package com.webkreator.qlue.util;
+
+import java.lang.reflect.Field;
+import java.security.InvalidParameterException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.xml.bind.DatatypeConverter;
+
+public class DateEditor implements PropertyEditor {
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Class getEditorClass() {
+		return Date.class;
+	}
+
+	@Override
+	public Date fromText(Field field, String text, Object currentValue) {
+		if (text == null) {
+			return (Date)currentValue;
+		}
+		
+		try {
+			return DatatypeConverter.parseDateTime(text).getTime();
+		} catch (IllegalArgumentException iae) {
+			throw new InvalidParameterException("qp.validation.date.invalid");
+		}		
+	}
+
+	@Override
+	public String toText(Object o) {		
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mmZ");			
+		return df.format((Date)o);
+	}
+}
