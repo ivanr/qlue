98d4a604fc57ac7e27accbbc8e4893bb8a720b2f
Ivan Ristic <ivanr@webkreator.com>
2016-02-19 18:05:19 +0100

    Tidy up.

--- src/com/webkreator/qlue/router/PackageRouter.java
+++ src/com/webkreator/qlue/router/PackageRouter.java
@@ -33,66 +33,66 @@ public class PackageRouter implements Router {
     private Log log = LogFactory.getLog(ClassRouter.class);
 
     private String packageName;
-	
-	protected RouteManager manager;
 
-	public PackageRouter(RouteManager manager, String packageName) {
-		this.manager = manager;
-		this.packageName = packageName;
-	}
+    protected RouteManager manager;
 
-	@Override
-	public Object route(TransactionContext tx, String routeSuffix) {
-		return resolveUri(tx, routeSuffix, packageName);
-	}
+    public PackageRouter(RouteManager manager, String packageName) {
+        this.manager = manager;
+        this.packageName = packageName;
+    }
 
-	/**
-	 * Returns page instance for a given URL.
-	 *  
-	 * @param path
-	 * @param rootPackage
-	 * @return page instance, or null if page cannot be found
-	 */
-	public Object resolveUri(TransactionContext tx, String path, String rootPackage) {
-		@SuppressWarnings("rawtypes")
-		Class pageClass = null;
+    @Override
+    public Object route(TransactionContext tx, String routeSuffix) {
+        return resolveUri(tx, routeSuffix, packageName);
+    }
 
-		if (path.indexOf("/../") != -1) {
-			throw new QlueSecurityException("Directory backreferences not allowed in path");
-		}
+    /**
+     * Returns page instance for a given URL.
+     *
+     * @param path
+     * @param rootPackage
+     * @return page instance, or null if page cannot be found
+     */
+    public Object resolveUri(TransactionContext tx, String path, String rootPackage) {
+        @SuppressWarnings("rawtypes")
+        Class pageClass = null;
 
-		// Start building class name.
-		StringBuilder sb = new StringBuilder();
+        if (path.indexOf("/../") != -1) {
+            throw new QlueSecurityException("Directory backreferences not allowed in path");
+        }
+
+        // Start building class name.
+        StringBuilder sb = new StringBuilder();
         String urlSuffix = null;
 
-		// Start with the root package.
-		sb.append(rootPackage);
+        // Start with the root package.
+        sb.append(rootPackage);
 
-		// Each folder in the URI corresponds to a package name.
-		String lastToken = null;
-		StringTokenizer st = new StringTokenizer(path, "/");
-		while (st.hasMoreTokens()) {
-			if (lastToken != null) {
+        // Each folder in the URI corresponds to a package name.
+        String lastToken = null;
+        StringTokenizer st = new StringTokenizer(path, "/");
+        while (st.hasMoreTokens()) {
+            if (lastToken != null) {
                 // We also don't allow path segments with periods, because
                 // they might interfere with class construction.
                 if (lastToken.indexOf('.') != -1) {
                     return null;
                 }
 
-				sb.append(".");
-				sb.append(lastToken);
-			}
+                sb.append(".");
+                sb.append(lastToken);
+            }
 
-			lastToken = st.nextToken();
-			
-			// We don't serve path segments whose names begin with $.
-			// Such packages are considered to be private.
-			if ((lastToken.length() > 0)&&(lastToken.charAt(0) == '$')) {
-				return null;
-			}
-		}
+            lastToken = st.nextToken();
 
-		if (lastToken != null) {
+            // We don't serve path segments whose names begin with $.
+            // Such packages are considered to be private.
+            if ((lastToken.length() > 0) && (lastToken.charAt(0) == '$')) {
+                return null;
+            }
+        }
+
+        if (lastToken != null) {
             // We allow one dot in the last segment, which could be a file suffix.
             int i = lastToken.indexOf('.');
             if (i != -1) {
@@ -101,50 +101,51 @@ public class PackageRouter implements Router {
                 log.debug("Detected suffix: " + urlSuffix);
             }
 
-			sb.append(".");
-			sb.append(lastToken);
-		}
+            sb.append(".");
+            sb.append(lastToken);
+        }
 
-		String className = sb.toString();
-		
-		log.debug("Trying class: " + className);
-		
-		// Look for a class with this name
-		pageClass = classForName(className);
-		if (pageClass == null) {
-			// We're here because we couldn't directly translate a request path
-			// into a class. So this might be directory access. But if we do
-			// find an index file, before we route to it we need to check if
-			// there is a terminating forward slash in the request URI. If not,
-			// we need to issue a redirect.
-			
-			// Look for the index page
-			pageClass = classForName(className + "." + manager.getIndex());
-			log.debug("Trying class: " + className + "." + manager.getIndex());
-			if (pageClass == null) {
-				// Not found, probably a 404
-				return null;
-			} else {
-				// We have found the index page
-				log.debug("Found index page");
-				
-				// Check if we need to issue a redirection
-				if (tx.getRequestUri().endsWith("/") == false) {
-					log.debug("Redirecting to " + tx.getRequestUri() + "/");
-					return new RedirectionRouter(tx.getRequestUri() + "/", 302).route(tx, path);
-				}
-			}
-		}
+        String className = sb.toString();
 
-		// Check class is instance of Page
-		if (!Page.class.isAssignableFrom(pageClass)) {
-			throw new RuntimeException("Class " + className + " is not a subclass of Page");
-		}
+        log.debug("Trying class: " + className);
+
+        // Look for a class with this name
+        pageClass = classForName(className);
+        if (pageClass == null) {
+            // We're here because we couldn't directly translate a request path
+            // into a class. So this might be directory access. But if we do
+            // find an index file, before we route to it we need to check if
+            // there is a terminating forward slash in the request URI. If not,
+            // we need to issue a redirect.
+
+            // Look for the index page
+            pageClass = classForName(className + "." + manager.getIndex());
+            log.debug("Trying class: " + className + "." + manager.getIndex());
+            if (pageClass == null) {
+                return null;
+            }
+
+            // We have found the index page
+            log.debug("Found index page");
+
+            // XXX Check suffixes match.
+
+            // Check if we need to issue a redirection
+            if (tx.getRequestUri().endsWith("/") == false) {
+                log.debug("Redirecting to " + tx.getRequestUri() + "/");
+                return new RedirectionRouter(tx.getRequestUri() + "/", 302).route(tx, path);
+            }
+        }
+
+        // Check that class is instance of Page
+        if (!Page.class.isAssignableFrom(pageClass)) {
+            throw new RuntimeException("Class " + className + " is not a subclass of Page");
+        }
 
         // Check that suffixes match.
 
         String pageSuffix = manager.getSuffix();
-        QlueMapping mapping = (QlueMapping)pageClass.getAnnotation(QlueMapping.class);
+        QlueMapping mapping = (QlueMapping) pageClass.getAnnotation(QlueMapping.class);
         if (mapping != null) {
             if (!mapping.suffix().equals("inheritAppSuffix")) {
                 pageSuffix = mapping.suffix();
@@ -174,33 +175,33 @@ public class PackageRouter implements Router {
             }
         }
 
-		try {
-			log.debug("Creating new instance of " + pageClass);
-			return pageClass.newInstance();
-		} catch (Exception e) {
-			log.error("Error creating page instance: " + e.getMessage(), e);
-			return null;
-		}
-	}
+        try {
+            log.debug("Creating new instance of " + pageClass);
+            return pageClass.newInstance();
+        } catch (Exception e) {
+            log.error("Error creating page instance: " + e.getMessage(), e);
+            return null;
+        }
+    }
 
-	/**
-	 * Returns class given its name.
-	 * 
-	 * @param name
-	 * @return
-	 */
-	@SuppressWarnings("rawtypes")
-	public static Class classForName(String name) {
-		try {
-			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-			return Class.forName(name, true, classLoader);
-		} catch (ClassNotFoundException e) {
-			return null;
-		} catch (NoClassDefFoundError e) {
-			// NoClassDefFoundError is thrown when there is a class
-			// that matches the name when ignoring case differences.
-			// We do not care about that.
-			return null;
-		}
-	}
+    /**
+     * Returns class given its name.
+     *
+     * @param name
+     * @return
+     */
+    @SuppressWarnings("rawtypes")
+    public static Class classForName(String name) {
+        try {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            return Class.forName(name, true, classLoader);
+        } catch (ClassNotFoundException e) {
+            return null;
+        } catch (NoClassDefFoundError e) {
+            // NoClassDefFoundError is thrown when there is a class
+            // that matches the name when ignoring case differences.
+            // We do not care about that.
+            return null;
+        }
+    }
 }
