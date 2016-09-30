7c847ac946271a0def3aa1448240ae738a4e4962
Ivan Ristic <ivanr@webkreator.com>
2016-09-30 09:50:49 +0100

    Explicitly specify input encoding when requesting templates. There seems to be a bug somewhere: even though we specify input.encoding in the properties file, Velocity still reverts to its default encoding when reading files.

--- src/com/webkreator/qlue/view/velocity/ClasspathVelocityViewFactory.java
+++ src/com/webkreator/qlue/view/velocity/ClasspathVelocityViewFactory.java
@@ -33,75 +33,75 @@ import java.util.StringTokenizer;
  */
 public class ClasspathVelocityViewFactory extends VelocityViewFactory {
 
-	/**
-	 * Initialize factory.
-	 */
-	@Override
-	public void init(QlueApplication qlueApp) throws Exception {
-		Properties properties = buildDefaultVelocityProperties(qlueApp);
-		log.info("Creating VelocityEngine with properties: " + properties);
-		velocityEngine = new VelocityEngine(properties);
-
-		// There appears to be some sort of problem with Velocity where it loads
-		// some classes using one classloader and some other classes using a different
-		// classloader. Because of that it is unable to match a perfectly valid
-		// class to an interface.
-		//
-		// Workaround taken from: https://github.com/whitesource/whitesource-bamboo-agent/issues/9
-		//
-		Thread thread = Thread.currentThread();
-		ClassLoader loader = thread.getContextClassLoader();
-		thread.setContextClassLoader(this.getClass().getClassLoader());
-		try {
-			SLF4JLogChute.setLoggingEnabled(false);
-			velocityEngine.getTemplate("FORCE_CLASSES_BE_LOADED_BY_THE_SAME_CLASSLOADER");
-		} catch(ResourceNotFoundException e) {
-			// This is expected, so ignore.
-		} finally {
-			thread.setContextClassLoader(loader);
-			SLF4JLogChute.setLoggingEnabled(true);
-		}
-	}
-
-	/**
-	 * Create a view instance, given page and view name. We use page class name
-	 * to create a folder hierarchy, finishing with the view name and the
-	 * suffix.
-	 */
-	@Override
-	public View constructView(Page page, String viewName) throws Exception {
-		StringBuilder sb = new StringBuilder();
-
-		StringTokenizer st = new StringTokenizer(page.getClass().getName(), ".");
-		String lastToken = null;
-
-		while (st.hasMoreTokens()) {
-			if (lastToken != null) {
-				sb.append('/');
-				sb.append(lastToken);
-			}
-
-			lastToken = st.nextToken();
-		}
-
-		sb.append('/');
-		sb.append(new File(viewName).getName());
-		sb.append(suffix);
-
-		return new VelocityView(this, velocityEngine.getTemplate(sb.toString()));
-	}
-
+    /**
+     * Initialize factory.
+     */
     @Override
-    public View constructView(String classpathName) throws Exception {
-        return new VelocityView(this, velocityEngine.getTemplate(classpathName));
+    public void init(QlueApplication qlueApp) throws Exception {
+        Properties properties = buildDefaultVelocityProperties(qlueApp);
+        log.info("Creating VelocityEngine with properties: " + properties);
+        velocityEngine = new VelocityEngine(properties);
+
+        // There appears to be some sort of problem with Velocity where it loads
+        // some classes using one classloader and some other classes using a different
+        // classloader. Because of that it is unable to match a perfectly valid
+        // class to an interface.
+        //
+        // Workaround taken from: https://github.com/whitesource/whitesource-bamboo-agent/issues/9
+        //
+        Thread thread = Thread.currentThread();
+        ClassLoader loader = thread.getContextClassLoader();
+        thread.setContextClassLoader(this.getClass().getClassLoader());
+        try {
+            SLF4JLogChute.setLoggingEnabled(false);
+            velocityEngine.getTemplate("FORCE_CLASSES_BE_LOADED_BY_THE_SAME_CLASSLOADER");
+        } catch (ResourceNotFoundException e) {
+            // This is expected, so ignore.
+        } finally {
+            thread.setContextClassLoader(loader);
+            SLF4JLogChute.setLoggingEnabled(true);
+        }
+    }
+
+    /**
+     * Create a view instance, given page and view name. We use page class name
+     * to create a folder hierarchy, finishing with the view name and the
+     * suffix.
+     */
+    @Override
+    public View constructView(Page page, String viewName) throws Exception {
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(page.getClass().getName(), ".");
+        String lastToken = null;
+
+        while (st.hasMoreTokens()) {
+            if (lastToken != null) {
+                sb.append('/');
+                sb.append(lastToken);
+            }
+
+            lastToken = st.nextToken();
+        }
+
+        sb.append('/');
+        sb.append(new File(viewName).getName());
+        sb.append(suffix);
+
+        return new VelocityView(this, velocityEngine.getTemplate(sb.toString(), inputEncoding));
     }
 
     @Override
-	protected Properties buildDefaultVelocityProperties(QlueApplication qlueApp) {
+    public View constructView(String classpathName) throws Exception {
+        return new VelocityView(this, velocityEngine.getTemplate(classpathName, inputEncoding));
+    }
+
+    @Override
+    protected Properties buildDefaultVelocityProperties(QlueApplication qlueApp) {
         Properties properties = super.buildDefaultVelocityProperties(qlueApp);
 
         String caching = properties.getProperty(RuntimeConstants.FILE_RESOURCE_LOADER_CACHE);
-        if ((caching == null)||(!Boolean.valueOf(caching))) {
+        if ((caching == null) || (!Boolean.valueOf(caching))) {
             properties.setProperty("class.resource.loader.class", "com.webkreator.qlue.view.velocity.NonCachingClasspathResourceLoader");
         } else {
             properties.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
