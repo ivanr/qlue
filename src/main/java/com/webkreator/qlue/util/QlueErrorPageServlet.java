2249df20951a1d0c21ba7b1c6334699c5372d5d1
Ivan Ristic <ivanr@webkreator.com>
2017-05-24 15:28:28 +0100

    Moved error page generation into TomcatErrorPageValve. This class needs to be available to the Tomcat server at startup, which means that we'll package it separately and thus needs to be self-contained.

--- src/main/java/com/webkreator/qlue/util/QlueErrorPageServlet.java
+++ src/main/java/com/webkreator/qlue/util/QlueErrorPageServlet.java
@@ -8,79 +8,8 @@ import java.io.*;
 
 public class QlueErrorPageServlet extends HttpServlet {
 
-    public static final String ERROR_PAGES_LOCATION = "/WEB-INF/error-pages/";
-
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        outputErrorPage(request, response);
-    }
-
-    public static void outputErrorPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        outputErrorPage(request, response, ERROR_PAGES_LOCATION);
-    }
-
-    public static void outputErrorPage(HttpServletRequest request, HttpServletResponse response, String errorPagesLocation) throws IOException {
-        int statusCode = response.getStatus();
-
-        // Servlet error properties of interest:
-        // - javax.servlet.error.exception
-        // - javax.servlet.error.status_code
-        // - javax.servlet.error.servlet_name
-        // - javax.servlet.error.request_uri
-        Exception exception = (Exception)request.getAttribute("javax.servlet.error.exception");
-        if (exception != null) {
-            statusCode = 500;
-        }
-
-        File errorFile = findErrorPage(request, statusCode, errorPagesLocation);
-        if (errorFile != null) {
-            response.setContentType("text/html; charset=utf-8");
-            sendFile(response, errorFile);
-        } else {
-            // No file, send the hardcoded error response.
-            String message = WebUtil.getStatusMessage(statusCode);
-            if (message == null) {
-                message = "Unknown Status Code (" + statusCode + ")";
-            }
-
-            response.setContentType("text/html; charset=utf-8");
-            PrintWriter out = response.getWriter();
-            out.print("<!DOCTYPE html>\n<html><head><title>");
-            out.print(HtmlEncoder.html(message));
-            out.println("</title></head>");
-            out.print("<body><h1>");
-            out.print(HtmlEncoder.html(message));
-            out.println("</h1>");
-            WebUtil.writePagePaddingforInternetExplorer(out);
-            out.println("</body></html>");
-        }
-    }
-
-    protected static File findErrorPage(HttpServletRequest request, int statusCode, String errorPagesLocation) {
-        File errorPagesHome = new File(request.getServletContext().getRealPath(errorPagesLocation));
-        File f = null;
-
-        f = new File(errorPagesHome, "error-" + statusCode + ".html");
-        if (f.exists() && f.canRead()) {
-            return f;
-        }
-
-        f = new File(errorPagesHome, "catch-all.html");
-        if (f.exists() && f.canRead()) {
-            return f;
-        }
-
-        return null;
-    }
-
-    protected static void sendFile(HttpServletResponse response, File file) throws IOException {
-        PrintWriter out = response.getWriter();
-        try (InputStream in = new FileInputStream(file);
-             BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                out.write(line);
-            }
-        }
+        TomcatErrorPageValve.outputErrorPage(request, response);
     }
 }
--- src/main/java/com/webkreator/qlue/util/TomcatErrorPageValve.java
+++ src/main/java/com/webkreator/qlue/util/TomcatErrorPageValve.java
@@ -5,19 +5,94 @@ import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ErrorReportValve;
 import org.apache.jasper.runtime.ExceptionUtils;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
 public class TomcatErrorPageValve extends ErrorReportValve {
+
+    public static final String ERROR_PAGES_LOCATION = "/WEB-INF/error-pages/";
 
     @Override
     protected void report(Request request, Response response, Throwable throwable) {
         try {
             String errorPagesLocation = System.getProperties().getProperty("QLUE_ERROR_PAGES");
             if (errorPagesLocation != null) {
-                QlueErrorPageServlet.outputErrorPage(request, response, errorPagesLocation);
+                outputErrorPage(request, response, errorPagesLocation);
             } else {
-                QlueErrorPageServlet.outputErrorPage(request, response);
+                outputErrorPage(request, response);
             }
         } catch(Exception e) {
             ExceptionUtils.handleThrowable(e);
+        }
+    }
+
+    public static void outputErrorPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        outputErrorPage(request, response, ERROR_PAGES_LOCATION);
+    }
+
+    public static void outputErrorPage(HttpServletRequest request, HttpServletResponse response, String errorPagesLocation) throws IOException {
+        int statusCode = response.getStatus();
+
+        // Servlet error properties of interest:
+        // - javax.servlet.error.exception
+        // - javax.servlet.error.status_code
+        // - javax.servlet.error.servlet_name
+        // - javax.servlet.error.request_uri
+        Exception exception = (Exception)request.getAttribute("javax.servlet.error.exception");
+        if (exception != null) {
+            statusCode = 500;
+        }
+
+        File errorFile = findErrorPage(request, statusCode, errorPagesLocation);
+        if (errorFile != null) {
+            response.setContentType("text/html; charset=utf-8");
+            sendFile(response, errorFile);
+        } else {
+            // No file, send the hardcoded error response.
+            String message = WebUtil.getStatusMessage(statusCode);
+            if (message == null) {
+                message = "Unknown Status Code (" + statusCode + ")";
+            }
+
+            response.setContentType("text/html; charset=utf-8");
+            PrintWriter out = response.getWriter();
+            out.print("<!DOCTYPE html>\n<html><head><title>");
+            out.print(HtmlEncoder.html(message));
+            out.println("</title></head>");
+            out.print("<body><h1>");
+            out.print(HtmlEncoder.html(message));
+            out.println("</h1>");
+            WebUtil.writePagePaddingforInternetExplorer(out);
+            out.println("</body></html>");
+        }
+    }
+
+    protected static File findErrorPage(HttpServletRequest request, int statusCode, String errorPagesLocation) {
+        File errorPagesHome = new File(request.getServletContext().getRealPath(errorPagesLocation));
+        File f = null;
+
+        f = new File(errorPagesHome, "error-" + statusCode + ".html");
+        if (f.exists() && f.canRead()) {
+            return f;
+        }
+
+        f = new File(errorPagesHome, "catch-all.html");
+        if (f.exists() && f.canRead()) {
+            return f;
+        }
+
+        return null;
+    }
+
+    protected static void sendFile(HttpServletResponse response, File file) throws IOException {
+        PrintWriter out = response.getWriter();
+        try (InputStream in = new FileInputStream(file);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                out.write(line);
+            }
         }
     }
 }
