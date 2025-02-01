3bfe240a300ec2d77279d8b54ef33b78fd4a73a1
Ivan Ristic <ivanr@webkreator.com>
2025-02-01 19:20:39 +0000

    Qlue: Better exception on startup failure.

--- src/main/java/com/webkreator/qlue/QlueServlet.java
+++ src/main/java/com/webkreator/qlue/QlueServlet.java
@@ -16,6 +16,9 @@
  */
 package com.webkreator.qlue;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.UnavailableException;
 import javax.servlet.annotation.MultipartConfig;
@@ -43,6 +46,8 @@ public class QlueServlet extends HttpServlet {
 
     private QlueApplication qlueApp;
 
+    private static final Logger log = LoggerFactory.getLogger(QlueServlet.class);
+
     /**
      * Retrieve servlet parameters from web.xml and initialize application.
      */
@@ -52,7 +57,7 @@ public class QlueServlet extends HttpServlet {
             createApplicationObject();
 
             if (qlueApp == null) {
-                throw new UnavailableException("QlueServlet: Application not available");
+                throw new UnavailableException("QlueServlet: Application not configured");
             }
 
             qlueApp.init(this);
@@ -60,8 +65,12 @@ public class QlueServlet extends HttpServlet {
             getServletContext().setAttribute(QLUE_SERVLET_INIT_FAILED, "true");
 
             if (e instanceof ServletException) {
+                // We assume the subclass is throwing exactly what
+                // it wants to communicate back to the servlet container.
                 throw (ServletException) e;
             } else {
+                // Otherwise, we tell the container that the app is unavailable.
+                log.error("Application failed to start", e);
                 throw new ServletException(e);
             }
         }
